5eafae9352656fbd183d930006ca37f1eaf66056
Joshua Ethridge <joshua.ethridge13@gmail.com>
2017-02-08 17:44:13 -0500

    Added in quit dialog box.

--- JChatServer.py
+++ JChatServer.py
@@ -1,21 +1,45 @@
 import tkinter
 from tkinter import *
 import tkinter.scrolledtext as ST
+import tkinter.messagebox as MB
+
+
+DEFAULT_PORT = 9966
+
 
 root = tkinter.Tk()
 verbose = IntVar()
 
+
 def kickUser():
     print("KICK")
 
+
 def quitConfirm():
-    print("Quit?")
+    confirm = MB.askquestion("Quit?", "Are you sure you want to quit?")
+    if confirm == "yes":
+        quitServer()
+
+
+def quitServer():
+    #TODO As program progresses, add additional shutdown logic.
+    sys.exit()
+
 
 def restartConfirm():
     print("Restart?")
 
+
+def startServer():
+    #TODO Start server
+
+    start["text"] = "Restart Server"
+    start.config(command = restartConfirm)
+
+
 def toggleVerbose():
     print(verbose.get())
+
 
 # GUI stuff
 verboseToggle = Checkbutton(root, text = "Verbose Output", variable = verbose, onvalue = 1, offvalue = 0, height = 5,
@@ -23,10 +47,10 @@ verboseToggle = Checkbutton(root, text = "Verbose Output", variable = verbose, o
 verboseToggle.grid(row = 0, column = 1)
 
 kick = Button(root, text = "Kick User", command = kickUser)
-kick.grid(row = 1, column = 1)
+kick.grid(row = 2, column = 1)
 
-restart = Button(root, text = "Restart Server", command = restartConfirm)
-restart.grid(row = 2, column = 1)
+start = Button(root, text = "Start Server", command = startServer)
+start.grid(row = 1, column = 1)
 
 quit = Button(root, text = "Quit", command = quitConfirm)
 quit.grid(row = 3, column = 1)
